8416fe23c9d6b771f741f72cd9ac77ac08e2edb6
Dharanivel Vicky <Rockv@DESKTOP-AB03V42.(none)>
2020-09-10 21:10:57 +0530

    added image slide

---
+++ public/js/indexscript.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $("#h").click(function(){
+        $(this).addClass("active");
+        $("#s").removeClass("active");
+        $("#c").removeClass("active");
+        $("#f").removeClass("active");
+    });
+    $("#s").click(function(){
+        $(this).addClass("active");
+        $("#h").removeClass("active");
+        $("#c").removeClass("active");
+        $("#f").removeClass("active");
+    });
+    $("#c").click(function(){
+        $(this).addClass("active");
+        $("#s").removeClass("active");
+        $("#h").removeClass("active");
+        $("#f").removeClass("active");
+    });
+    $("#f").click(function(){
+        $(this).addClass("active");
+        $("#s").removeClass("active");
+        $("#h").removeClass("active");
+        $("#c").removeClass("active");
+    })
+});
